c4e61825af5f83ec15269caab0f31129c27ce126
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-12-10 21:58:08 +0100

    Editing script

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 tsc
 electron-packager . Homemade_F1MV_BETA0.1 --platform=win32 --arch=x64 --ignore=/node_modules
 rm -rf *.js
-cd Homemade_F1MV_BETA0
+cd Homemade_F1MV_BETA0.1-win32-x64
 npm i
 rm -rf *.ts
 rm -rf variables.js
 rm -rf .env
 echo "PATH_STATIC=\"PROD\"" > .env
+cd ../
