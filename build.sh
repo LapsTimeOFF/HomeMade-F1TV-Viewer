63dff972efba0a4a7ff51a4080ff1e1b92115be5
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-12-10 22:09:54 +0100

    Editing script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+rm -rf Homemade_F1MV_*
 tsc
 electron-packager . Homemade_F1MV_BETA0.1 --platform=win32 --arch=x64 --ignore=/node_modules
 rm -rf *.js
-cd Homemade_F1MV_BETA0.1-win32-x64
-npm i
+cd Homemade_F1MV_BETA0.1-win32-x64/resources/app
+pnpm i
 rm -rf *.ts
 rm -rf variables.js
 rm -rf .env
